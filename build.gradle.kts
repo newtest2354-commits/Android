d983bfa169e3497fee3f3995cd4e628a20b17fff
Bb <newtest2354@gmail.com>
2026-06-27 00:10:14 +0330

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-}
